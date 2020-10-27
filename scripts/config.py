e463c1eaa5bfe6ddbda8112964a34f06a862347f
Sunrise <qlmlove@foxmail.com>
2020-10-27 15:34:45 +0800

    pinesnow
    init project

---
+++ scripts/config.py
@@ -0,0 +1,6 @@
+class Config：
+    root = 'notes'  # 目录位置
+    title = '各种学习笔记'
+    source_dir = 'res'
+    template_dir= 'templates'
+    
---
+++ words.md
@@ -0,0 +1,7 @@
+# 一些用到但可能不认识的单词
+
+generate 生成
+SoftwareTest 软件测试
+environment_construction 环境搭建
+programing_language 编程语言
+explain 说明
